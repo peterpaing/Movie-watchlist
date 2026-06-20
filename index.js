0434a23775b0d10cf747ce378e81271e7776f6c1
PyaeSonePaing-104 <peterpaingcoding@gmail.com>
2026-06-20 22:06:34 +0630

    get movie poster and title from Api

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 document.querySelector("form").addEventListener("submit", (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    const inputEl = document.getElementById('search-input').value
+    const inputEl = document.getElementById('search-input').value;
 
     fetch(`https://www.omdbapi.com/?s=${encodeURIComponent(inputEl)}&apikey=f0eeb5ed`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+
+            if (data.Response === "False") {
+                document.getElementById('movie-list').innerHTML =
+                    `<p>No results found</p>`
+                return
+            }
+
+            const movieList = data.Search.map(movie => {
+                return `
+                    <div class="movie">
+                        <img src="${movie.Poster}" alt="${movie.Title}">
+                        <div class="movie-details">
+                            <h3>${movie.Title}</h3>
+                            <div class="about-movie">
+                                <p>${movie.Year}</p>
+                                <i class="fa-solid fa-circle-plus"></i>
+                                <p>Watchlist</p>
+                            </div>
+                        </div>
+                    </div>
+                `
+            }).join('')
+
+            document.getElementById('movie-list').innerHTML = movieList
+        })
+        .catch(err => {
+            console.error(err)
+        })
 })
